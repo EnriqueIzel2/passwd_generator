32e4ca08b36d702b4b2d06edb2a4c274e8558d11
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-25 14:57:22 -0400

    add array of letters

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -7,6 +7,11 @@ import com.example.passwordgenerator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
 
+  private val alphabet = arrayOf(
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+    "O", "P", "K", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+  )
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
