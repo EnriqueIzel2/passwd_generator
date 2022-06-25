4e1032d8037fe4d53faf6f1f41e26c60427b614a
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-25 15:16:56 -0400

    create function to generate password, add the new password in the TextView

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.passwordgenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.passwordgenerator.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
@@ -16,5 +17,21 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    binding.btnGeneratePasswd.setOnClickListener {
+      binding.textPassword. text = generatePassword()
+    }
+  }
+
+  private fun generatePassword(): String {
+    val firstChoice = (alphabet.indices).random()
+    val secondChoice = (alphabet.indices).random()
+
+    val firstLetter = alphabet[firstChoice]
+    val secondLetter = alphabet[secondChoice]
+
+    val numbers = Random.nextInt(1000, 9999)
+
+    return "$firstLetter$secondLetter-$numbers"
   }
 }
